c404a6c2961938a925965d1fc114acdab2ba065b
Leo Petchey <petchey47leo@gmail.com>
2025-09-26 19:47:17 +0100

    Identifies word types, created consistent file structure. Standardises word to corpus format.

--- TestCounter.py
+++ TestCounter.py
@@ -1,15 +1,92 @@
+import re
+import os
 
-items = ['apple', 'red', 'apple', 'red', 'red', 'pear']
+
+
+
+items = ['apple', 'red', 'apple', 'red', 'red', 'pear', 'Capital', 'They\'re', 'half-hearted', 'Half-hearted', 'they\'re', 'I', 'I\'m', 'can\'t' ]
 
 
 counts = dict()
 for i in items:
-    print("i is :", i)
-    print("counts.get(i, 0) is:", counts.get(i, 0))
+    #print("i is :", i)
+    #print("counts.get(i, 0) is:", counts.get(i, 0))
     #print("counts[i] is :", counts[i])
     counts[i] = counts.get(i, 0) + 1
-    print("counts is :", counts)
+    #print("counts is :", counts)
 
 
-    val_based_rev = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
+#    val_based_rev = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
+
+
+
+print(counts)
+key_list = list(counts.keys())
+potential_proper_nouns = [words for words in counts.keys() if re.match(r"[A-Z][a-z'’-]*", words)]
+print("Potential proper nouns", potential_proper_nouns)
+
+
+print(key_list)
+key_list = [words.replace("’", "'") for words in key_list]
+print(key_list)
+key_list = [words.replace("n't", " not") for words in key_list]
+print(key_list)
+key_list = [words.replace("'", " '") for words in key_list]
+print(key_list)
+key_list = [words.replace("-", " - ") for words in key_list]
+print(key_list)
+
+
+with open(r"Corpus Data\Refined Data\refined_corpus_data_file", 'r') as f:
+    print(f.readline())
+
+
+folder = os.scandir(r"Corpus Data\Total Counts")
+
+for file in folder:
+    print(file)
+
+x1 = 2
+x2 = 3
+x3 = 7
+
+
+for n in range(1,4):
+    print(n)
+
+test = "aAbt't-rw’f G"
+print(test.casefold().split())
+print(type("bob".split()))
+
+
+list1 = [1,2,4]
+list2 = [1,2,4]
+
+print(list1 == list2)
+
+def add(a,b):
+    result = int(a)+int(b)
+    return result
+
+print(add(1,1))
+
+Fun = (add(1,2), add(4,7))
+print(Fun)
+
+n = 5
+
+print("Bob" + str(n+1))
+
+dictionary = {"Car": "Broken", "Age": 10}
+for key in dictionary.keys():
+    print(key)
+
+k = "Don't"
+k = k.replace("D","W")
+print(k)
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import csv
+import os
 import ebooklib
 from ebooklib import epub
 import collections
@@ -11,82 +11,150 @@ from bs4 import BeautifulSoup
 # Corpus info from: English Fiction Version 20200217
 # https://storage.googleapis.com/books/ngrams/books/20200217/eng-fiction/eng-fiction-1-ngrams_exports.html
 
-corpusDataFilepath = "1-00000-of-00001"
-corpusCountFilepath = "totalcounts-1"
-epubFilepath1 = "short_fiction_story.epub"
-epubFilepath2 = "Waybound Cradle Book 12 Will Wight Z-Library.epub"
-epubFilepath3 = "pg76268-images-3.epub"
+corpus_ngram_data_folder_paths = (r"Corpus Data/1-gram Data",
+                                  r"Corpus Data/2-gram Data",
+                                  r"Corpus Data/3-gram Data")
+refined_corpus_ngram_data_file_paths = (r"Corpus Data/Refined Data/refined 1-gram data file",
+                                        r"Corpus Data/Refined Data/refined 2-gram data file",
+                                        r"Corpus Data/Refined Data/refined 3-gram data file")
+corpus_counts_path = r"Corpus Data/Total Counts"
+epub_path = r"Input Documents"
 initialSearchYear = 2010
 finalSearchYear = 2019
 
+def refine_corpus_data(words_data):
+    standardised_words = [standardise_word(word) for word in list(words_data)]
+    word_list_1_grams = [words for words in standardised_words if len(words.split()) == 1]
+    word_list_2_grams = [words for words in standardised_words if len(words.split()) == 2]
+    word_list_3_grams = [words for words in standardised_words if len(words.split()) == 3]
+    word_n_gram_tuple = (word_list_1_grams, word_list_2_grams, word_list_3_grams)
+    for n in range(1,4):
+        refine_data_in_folder(word_n_gram_tuple[n-1], n)
+        print(f"Refined corpus {n}-gram data")
+    return
 
-def get_corpus_data(corpus_data_filepath, corpus_count_filepath):
-    # Assigns an index to each word in the data, allows us to find the position of words in the full dataframe while being
-    # much smaller than the full dataframe
-    corpus_words_df = pd.read_table(corpus_data_filepath, usecols=[0], header=None, names=["Word"], quoting=3, dtype=str)
-    #TODO possibly change quote character to quotechar='\x07'
+def refine_data_in_folder(word_list, n_value):
+    folder_path = corpus_ngram_data_folder_paths[n_value - 1]
+    refined_file_path = refined_corpus_ngram_data_file_paths[n_value - 1]
+    if os.path.exists(refined_file_path):
+        os.remove(refined_file_path)
+    folder = os.scandir(folder_path)
+    for file in folder:
+        words_df = pd.read_table(file, usecols=[0], header=None, names=["Word"], quoting=3, dtype=str)
+        refined_corpus_indices = words_df.index[(words_df["Word"].str.casefold().isin(word_list))]
 
-    # Reads the total counts data and find the total number of words that appeared from initialSearchYear to finalSearchYear
-    total_counts_df = pd.read_csv(corpus_count_filepath, header=None, lineterminator="\t", dtype=int,
-                                  names=["Year", "Word_Count", "Page_Count", "Volume_Count"])
-    selected_years_counts_df = total_counts_df.loc[total_counts_df["Year"].ge(initialSearchYear) &
-                                                   total_counts_df["Year"].le(finalSearchYear), ["Year", "Word_Count"]]
-    total_count_value = selected_years_counts_df["Word_Count"].sum()
-    return corpus_words_df, total_count_value
+        with open(file, 'r', encoding='utf-8') as data, open(refined_file_path, 'a', encoding='utf-8') as refined_data:
+            for i, line in enumerate(data):
+                if i in refined_corpus_indices:
+                    refined_data.write(line)
+        #
+        #     refined_corpus_words_df = refined_corpus_words_df.reset_index(drop=True)
+        #
+        #     return refined_corpus_words_df, refined_corpus_data_filepath
 
 
-def refine_corpus_data(book_word_counts, corpus_words_df, corpus_data_filepath):
-    refined_corpus_data_filepath = 'refined_corpus_data_file'
 
-    book_word_list = [word.casefold() for word in list(book_word_counts)]
-    refined_corpus_indices = corpus_words_df.index[(corpus_words_df["Word"].str.casefold().isin(book_word_list))]
 
-    refined_corpus_words_df = corpus_words_df.iloc[refined_corpus_indices]
+        # with (open(file, 'r', encoding='utf-8') as data,
+        #       open(refined_file_path, 'a', encoding='utf-8') as refined_data):
+        #     counter = 0
+        #     for line in data:
+        #         print(f"Line number {counter}")
+        #         word = line.split()[:n_value]
+        #         word = [word_part.casefold for word_part in word]
+        #         if word in words_list:
+        #             refined_data.write(line)
+        #         counter += 1
 
-    with open(corpus_data_filepath, 'r', encoding='utf-8') as data, open(refined_corpus_data_filepath, 'w', encoding='utf-8') as refined_data:
-        for i, line in enumerate(data):
-            if i in refined_corpus_indices:
-                refined_data.write(line)
 
-    refined_corpus_words_df = refined_corpus_words_df.reset_index(drop=True)
+    return
 
-    return refined_corpus_words_df, refined_corpus_data_filepath
+def standardise_word(word):
+    standard_word = word.casefold()
+    standard_word = standard_word.replace("’", "'")
+    if standard_word == "ain't":
+        standard_word = "is not"
+    elif standard_word == "can't":
+        standard_word = "can not"
+    elif standard_word == "shan't":
+        standard_word = "shall not"
+    elif standard_word == "won't":
+        standard_word = "will not"
+    standard_word = standard_word.replace("n't", " not")
+    standard_word = standard_word.replace("'", " '")
+    standard_word = standard_word.replace("-", " - ")
+    return standard_word
+
+
+# def get_corpus_data(corpus_data_filepath, corpus_count_filepath):
+#     # Assigns an index to each word in the data, allows us to find the position of words in the full dataframe while being
+#     # much smaller than the full dataframe
+#     corpus_words_df = pd.read_table(corpus_data_filepath, usecols=[0], header=None, names=["Word"], quoting=3, dtype=str)
+#     #TODO possibly change quote character to quotechar='\x07'
+#
+#     # Reads the total counts data and find the total number of words that appeared from initialSearchYear to finalSearchYear
+#     total_counts_df = pd.read_csv(corpus_count_filepath, header=None, lineterminator="\t", dtype=int,
+#                                   names=["Year", "Word_Count", "Page_Count", "Volume_Count"])
+#     selected_years_counts_df = total_counts_df.loc[total_counts_df["Year"].ge(initialSearchYear) &
+#                                                    total_counts_df["Year"].le(finalSearchYear), ["Year", "Word_Count"]]
+#     total_count_value = selected_years_counts_df["Word_Count"].sum()
+#     return corpus_words_df, total_count_value
+#
+
+
+
+# def refine_corpus_data(book_word_counts, corpus_words_df, corpus_data_filepath):
+#     refined_corpus_data_filepath = 'refined_corpus_data_file'
+#
+#     book_word_list = [word.casefold() for word in list(book_word_counts)]
+#     refined_corpus_indices = corpus_words_df.index[(corpus_words_df["Word"].str.casefold().isin(book_word_list))]
+#
+#     refined_corpus_words_df = corpus_words_df.iloc[refined_corpus_indices]
+#
+#     with open(corpus_data_filepath, 'r', encoding='utf-8') as data, open(refined_corpus_data_filepath, 'w', encoding='utf-8') as refined_data:
+#         for i, line in enumerate(data):
+#             if i in refined_corpus_indices:
+#                 refined_data.write(line)
+#
+#     refined_corpus_words_df = refined_corpus_words_df.reset_index(drop=True)
+#
+#     return refined_corpus_words_df, refined_corpus_data_filepath
 
 #Given a word, it first finds all the indices matching this word (case-insensitive), next it reads the data
 #corresponding to each of these indices and sums the frequencies of the word from initialSearchYear to finalSearchYear.
 #It returns the sum of all these frequencies
-def corpus_word_freq(input_word, corpus_word_indices_df, corpus_data_filepath):
+def corpus_word_freq(input_word, corpus_word_indices_df_triple):
     total_word_freq = 0
+    standard_input_word = standardise_word(input_word)
+    n = len(standard_input_word.split())
+    if n not in [0,1,2]:
+        return 0, 1
+    else:
+        corpus_word_indices_df = corpus_word_indices_df_triple[n-1]
+        corpus_data_filepath = refined_corpus_ngram_data_file_paths[n-1]
 
-    #words.replace("’", "'")
-    #words.replace("n't", " not")
-    #words.replace("'", " '")
-    #words.replace("-", " - ")
+        word_indices = corpus_word_indices_df.index[(corpus_word_indices_df["Word"].str.casefold() == standard_input_word)]
+        col_names = list(range(1000))       # Creates excess columns to ensure all the data is extracted
 
+        num_of_words = list(range(len(corpus_word_indices_df.index)))
+        skip_indices = [nums for nums in num_of_words if nums not in word_indices]
 
+        word_data = pd.read_table(corpus_data_filepath, header=None, quoting=3, names=col_names,
+                                  skiprows=skip_indices, nrows=len(word_indices), index_col=0, dtype=str)
+        word_data = word_data.dropna(axis=1, how='all')
+        word_data = word_data.T
 
-    word_indices = corpus_word_indices_df.index[(corpus_word_indices_df["Word"].str.casefold() == input_word.casefold())]
-    col_names = list(range(1000))       # Creates excess columns to ensure all the data is extracted
+        for col in word_data:  #TODO rename variables
+            temp_word_data = word_data[col].dropna()
 
-    num_of_words = list(range(len(corpus_word_indices_df.index))) #TODO take num_of_words as input?
-    skip_indices = [nums for nums in num_of_words if nums not in word_indices]
+            word_data_selected_years = temp_word_data[(temp_word_data.str[:4].astype(int).ge(initialSearchYear) &
+                                                  temp_word_data.str[:4].astype(int).le(finalSearchYear))]
+            word_freq_selected_years = word_data_selected_years.str.split(",").str[1].astype(int)
+            word_freq = sum(word_freq_selected_years)
+            total_word_freq += word_freq
 
-    word_data = pd.read_table(corpus_data_filepath, header=None, quoting=3, names=col_names,
-                              skiprows=skip_indices, nrows=len(word_indices), index_col=0, dtype=str)
-    word_data = word_data.dropna(axis=1, how='all')
-    word_data = word_data.T
-
-    for col in word_data:  #TODO rename variables
-        temp_word_data = word_data[col].dropna()
-
-        word_data_selected_years = temp_word_data[(temp_word_data.str[:4].astype(int).ge(initialSearchYear) &
-                                              temp_word_data.str[:4].astype(int).le(finalSearchYear))]
-        word_freq_selected_years = word_data_selected_years.str.split(",").str[1].astype(int)
-        word_freq = sum(word_freq_selected_years)
-        total_word_freq += word_freq
-
-    print("Word =", input_word, "and count =", total_word_freq)
-    return total_word_freq
+        print("Word =", input_word, "and count =", total_word_freq)
+        return total_word_freq, n
 
 # Opens an epub file and returns the text from each chapter as a single string
 def epub_to_str(epub_filepath):
@@ -107,13 +175,14 @@ def str_to_counted_data(book_string):
     word_list = re.findall(r"\b[a-zA-Z][a-zA-Z-'’]*s['’]|[a-zA-Z][a-zA-Z-'’]*\b", book_string)
     #word_list = re.findall(r"\b\w+(?:['’]\w+)*\b", string)
     #word_list = re.findall(r"\b(([a-zA-Z-'’])+s['’]|([a-zA-Z-'’])+\b)", string)
-    proper_nouns = re.findall(r"(?<![.!?] )\b[A-Z][a-z'’-]*\b", book_string)
+    proper_nouns = re.findall(r"(?<![.!?] )\b[A-Z][a-z'’-]*\b", book_string) #TODO can't handle words of the form "Foo-Bar"
 
     full_counted_words = collections.Counter(word_list)
     counted_proper_nouns = collections.Counter(proper_nouns)
+    counted_proper_nouns.pop("I", None)
 
-    filtered_counted_words = full_counted_words
-    for key in counted_proper_nouns.keys():
+    filtered_counted_words = full_counted_words.copy()
+    for key in counted_proper_nouns:
         filtered_counted_words[key] = full_counted_words[key] - counted_proper_nouns[key]
         if filtered_counted_words[key] == 0:
             del filtered_counted_words[key]
@@ -123,9 +192,10 @@ def str_to_counted_data(book_string):
     merged_filtered_counted_words = merge_word_data(filtered_counted_words)
 
     #potential_proper_nouns = [words for words in merged_counted_words.keys() if re.match(r"[A-Z][a-z'’-]*", words)]
-    return merged_full_counted_words, merged_filtered_counted_words, proper_nouns
+    return merged_full_counted_words, merged_filtered_counted_words, counted_proper_nouns
 
 def merge_word_data(counted_word_data):
+    counted_word_data = {key: value for key, value in sorted(counted_word_data.items(), key=lambda item: item[1], reverse=True)}
     merged_words_data = {}
     temp_key_list = []
     for key in counted_word_data:
@@ -138,26 +208,34 @@ def merge_word_data(counted_word_data):
             temp_key_list.append(key.casefold())
     return merged_words_data
 
-def create_words_df(sorted_words_data, corpus_data_filepath, corpus_counts_filepath):
-    corpus_word_indices_df, corpus_total_count = get_corpus_data(corpus_data_filepath, corpus_counts_filepath)
-    print("Corpus data obtained")
+def create_words_df(sorted_words_data,filtered_sorted_words_data):
+    corpus_words_df_list = []
+    corpus_total_count_list = []
+    for n in range(1,4):
+        corpus_words_df_list.append(pd.read_table(refined_corpus_ngram_data_file_paths[n-1], usecols=[0], header=None, names=["Word"], quoting=3, dtype=str))
+        total_counts_df = pd.read_csv("Corpus Data/Total Counts/totalcounts-" + str(n), header=None, lineterminator="\t", dtype=int,
+                                                                         names=["Year", "Word_Count", "Page_Count", "Volume_Count"])
+        selected_years_counts_df = total_counts_df.loc[total_counts_df["Year"].ge(initialSearchYear) &
+                                                                                          total_counts_df["Year"].le(finalSearchYear), ["Year", "Word_Count"]]
+        corpus_total_count_list.append(selected_years_counts_df["Word_Count"].sum())
+
     book_total_count = sum(list(sorted_words_data.values()))
 
-    print(corpus_word_indices_df.info())
-    corpus_word_indices_df, corpus_data_filepath = refine_corpus_data(sorted_words_data, corpus_word_indices_df, corpus_data_filepath)
-    print(corpus_word_indices_df.info())
+    #print(corpus_word_indices_df.info())
+    #corpus_word_indices_df, corpus_data_filepath = refine_corpus_data(sorted_words_data, corpus_word_indices_df, corpus_data_filepath)
+    #print(corpus_word_indices_df.info())
 
     transposed_words_df_data = {}
-    for key, value in sorted_words_data.items():
-        print(key)
-        corpus_freq = corpus_word_freq(key, corpus_word_indices_df, corpus_data_filepath)
-        book_relative_freq = value/book_total_count
-        corpus_relative_freq = corpus_freq/corpus_total_count
-        relative_freq_ratio = (value * corpus_total_count)/(corpus_freq * book_total_count)
+    for word, book_freq in sorted_words_data.items():
+        #book_filtered_freq =
+        corpus_freq, n = corpus_word_freq(word, corpus_words_df_list)
+        book_relative_freq = book_freq/book_total_count
+        corpus_relative_freq = corpus_freq/corpus_total_count_list[n-1]
+        relative_freq_ratio = (book_freq * corpus_total_count_list[n-1])/(corpus_freq * book_total_count)
 
-        transposed_words_df_data.update({key: [value, book_relative_freq, corpus_freq,
+        transposed_words_df_data.update({word: [book_freq, book_relative_freq, corpus_freq,
                                                corpus_relative_freq, relative_freq_ratio]})
-        print(transposed_words_df_data[key])
+        print(transposed_words_df_data[word])
 
     transposed_words_df = pd.DataFrame(transposed_words_df_data, index=["Book Frequency",
                                                                         "Book Relative Frequency",
@@ -175,35 +253,33 @@ def df_to_excel(input_df, file_name):
 
 
 
-def epub_to_excel(epub_filepath, corpus_data_filepath, corpus_counts_filepath):
+def epub_to_excel(epub_filepath):
     print("Starting process")
     book_string = epub_to_str(epub_filepath)
     print("Book word list obtained")
-    merged_full_counted_words, merged_filtered_counted_words, proper_nouns = str_to_counted_data(book_string)
+    merged_full_counted_words, merged_filtered_counted_words, counted_proper_nouns = str_to_counted_data(book_string)
     print("Sorted word data obtained")
+    refine_corpus_data(merged_full_counted_words)
+    print("Corpus data refined")
 
-    final_words_df = create_words_df(merged_full_counted_words, corpus_data_filepath, corpus_counts_filepath)
-    df_to_excel(final_words_df, "WordFrequencies2.xlsx")
+    final_words_df = create_words_df(merged_full_counted_words, merged_filtered_counted_words)
+    df_to_excel(final_words_df, "WordFrequencies3.xlsx")
     print(final_words_df)
     return
 
 
-epub_to_excel(epubFilepath2, corpusDataFilepath, corpusCountFilepath)
-
-#TODO fix file names for excel output and data files
+epub_to_excel(r"Input Documents/Waybound Cradle Book 12 Will Wight Z-Library.epub")
 
 
 
+#TODO fix file names for excel output
 #TODO Makes nice plots????
 
 #TODO Send to multiple pages: full data, data w/out inf, data w/out inf and proper nouns (also pages to show proper nouns and inf words)
-#TODO identify proper noun if they only occur capitalised
+#TODO consider harsh + lenient proper noun removal
 #TODO Read txt files as well as all files from a folder (web scraping?)
-#TODO handle corpus data bigger than 1 file
-#TODO words with apostrophes from 2-grams and hyphenated words from 3 grams
 #TODO write 2 separate files to preprocess 2/3-grams
 #TODO total counts into excel
-#TODO Filepaths as inputs
 #TODO Verify filepaths
 
 
